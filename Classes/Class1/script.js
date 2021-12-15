65bc9813bb623075a4e50d968b8b060eb7a3b80e
anetastankovska <anetastankovskaane@gmail.com>
2021-12-15 21:14:47 +0100

    Created Classes folder Created Homeworks folder Added code from class 1

---
+++ Classes/Class1/script.js
@@ -0,0 +1,116 @@
+console.log("Hello From JavaScript!");
+console.log("Our second log");
+
+
+let number;
+number = 5;
+
+console.log(number);
+
+let pi = 3.141592653589793;
+
+5 + pi;
+
+let a = "Trajan Stevkovski";
+let trainerName = "Trajan Stevkovski";
+let b = 33;
+let trainetAge = 33;
+let abc = "";
+let trainerFirstNameAndLastName = "Trajan Stevkovski";
+
+let isValid = true;
+
+let justDeclared;
+console.log("JustDeclared");
+console.log(justDeclared);
+console.log(typeof justDeclared);
+
+let nullDefined = null;
+console.log("nullDefined");
+console.log(nullDefined);
+console.log(typeof nullDefined);
+
+let sumOfTwoNumbers = "3 + 7 =10";
+console.log(sumOfTwoNumbers);
+console.log(typeof sumOfTwoNumbers);
+
+
+
+let num1 = 1.123;
+console.log(num1);
+console.log(typeof num1);
+
+
+// Single line comment
+// This variable is the name of the trainer - Not good use of the comments
+let asd = "Trajan Stevkovski"
+
+// Multi line comment
+/* Validating a password that should contain minimum 8 character and maximum 16 character and should also contain minimum one capital letter and one special character  - VALID COMMENT */
+
+let numberFour;
+console.log(numberFour) // this uses variable
+console.log(4) // this uses explicit value
+
+console.log("this is the value of variable named numberFour", numberFour);
+
+console.log("----------------------------ARITMETHIC OPERATORS");
+
+let sum = 3 +2;
+console.log(sum);
+
+let subtract = sum - 2; 
+console.log(subtract);
+
+let multiply = subtract * sum; 
+console.log(multiply);
+
+let division = 10/2;
+console.log(division);
+
+let modulus = 9 % 2;
+console.log(modulus);
+
+let increment = 3;
+increment++;
+console.log();
+
+let decrement = 3;
+decrement--;
+console.log();
+
+console.log("---------------------------- COMPARISON OPERATORS")
+
+let isLarger = 5 > 5;
+console.log(isLarger);
+
+let isLargerOrEven = 5>= 5;
+console.log(isLargerOrEven);
+
+let isSmaller = 3 < 3;
+console.log(isSmaller);
+
+let isSmallerOrEven = 3 <= 3;
+console.log(isSmallerOrEven);
+
+let number5 = 3;
+let number6 = 5;
+let isLargerWithNewVariables = number5 < number6; // 3 < 5 = true
+console.log(isLargerWithNewVariables);
+
+
+console.log("-----------------------------ASSIGNMENT OPERATORS");
+
+let num2 = 2;
+let num3 = 3;
+
+num2 = num2 + num3; //2 + 3 = 5
+console.log(num2);
+num2 += num3; //5 + 3 = 8
+console.log(num2);
+
+num2 = num2 - num3; //8 - 3 = 5
+num2 = num2 += num3; // 5 - 3 = 2
+console.log(num2);
+
+
